6605ec4e38899aeb3a9aacce077a893df32ad36c
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:52:02 -0300

    Create ex41.c

---
+++ lab02/ex41.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float v, h, s;
+    printf("Digite o valor da hora de trabalho em reais \n");
+    scanf("%f", &v);
+    printf("Digite o numero de horas trabalhadas no mes \n");
+    scanf("%f", &h);
+    s = (v * h) * 1.1;
+    printf("O valor que voce vai receber eh %f", s);
+    return 0;
+}
