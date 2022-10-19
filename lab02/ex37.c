f88d60f4145e1ed68e7453e88491dd6c0dec1bc9
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:49:10 -0300

    Create ex37.c

---
+++ lab02/ex37.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main()
+{
+    float p, d;
+    printf("Digite um valor de um produto\n");
+    scanf("%f", &p);
+    d= p*0.88;
+    printf("valor com desconto e %f", d);
+    return 0;
+}
