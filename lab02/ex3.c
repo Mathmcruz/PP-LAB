7f3dc5e96c8352334bcd259923dad21dcae976fc
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:27:46 -0300

    Create ex3.c

---
+++ lab02/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1, n2, n3, soma;
+    printf("digite tres numeros inteiros\n");
+    scanf("%d %d %d", &n1, &n2, &n3);
+    soma = n1+ n2+ n3;
+    printf("a soma e %d", soma);
+    return 0;
+}
