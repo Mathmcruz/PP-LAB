c981afc5f9b8c12e3d2157129abcee446941a151
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:21:24 -0300

    Create ex2.c

---
+++ lab02/ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num;
+    printf("digite um numero real\n");
+    scanf("%f", &num );
+    printf("o mumero que voce digitou %f", num);
+    return 0;
+}
