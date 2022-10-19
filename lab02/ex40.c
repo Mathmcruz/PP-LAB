7e5633d50d455194ea761bc7088747e5fd51c4c4
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:51:31 -0300

    Create ex40.c

---
+++ lab02/ex40.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main()
+{
+    float d, l;
+    printf("Digite o numero de dias trabalhados pelo encanador\n");
+    scanf("%f", &d);
+    l= 27.6*d;
+    printf("valor liquido a ser pago e %f", l);
+    return 0;
+}
