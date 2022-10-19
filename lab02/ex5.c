ede75920d5cc868d1eda61d55406a9a51ad3af53
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:28:31 -0300

    Create ex5.c

---
+++ lab02/ex5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num, qui;
+    printf("digite um numero real\n");
+    scanf("%f", &num );
+    qui= num/5;
+    printf("um quinto eh %f", qui);
+    return 0;
+}
