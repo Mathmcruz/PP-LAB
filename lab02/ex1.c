4f122de4a7df069b90f523d9f2bd2a1362bd94ce
Mathmcruz <115501111+Mathmcruz@users.noreply.github.com>
2022-10-19 13:20:19 -0300

    Create ex1.c

---
+++ lab02/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("digite um numero inteiro\n");
+    scanf("%d", &num );
+    printf("o mumero que voce digitou %d", num);
+    return 0;
+}
